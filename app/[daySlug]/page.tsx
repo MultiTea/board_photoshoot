7638115753fb797ae09fea2a3dbf4b16d7d6cf48
MultiTea <lucasbolea@live.fr>
2024-10-07 00:41:05 +0200

    static params

--- app/[daySlug]/page.tsx
+++ app/[daySlug]/page.tsx
@@ -9,3 +9,19 @@ export default function DayPage({ params }: { params: { daySlug: string } }) {
     </div>
   );
 }
+
+export async function generateStaticParams() {
+  const response = await fetch(
+    'https://data.fblacklight.org/api/events?filters[id][$in][0]=252&filters[id][$in][1]=253&filters[id][$in][2]=254&filters[id][$in][3]=255&populate=*'
+  );
+  const data = await response.json();
+
+  return data.data.map((event: any) => {
+    const date = new Date(event.attributes.start_date);
+    const dayName = date.toLocaleDateString('fr-FR', { weekday: 'long' });
+    const dayNumber = date.getDate();
+    return {
+      daySlug: `${dayName}-${dayNumber}`,
+    };
+  });
+}
